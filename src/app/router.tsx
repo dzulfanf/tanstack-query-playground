a2a2efb83e12d3356c2749bdc02ff14f8ffa78ac
dzulfanfadli <dzulfan.dev@gmail.com>
2026-07-30 10:08:44 +0700

    feat: wire module-02 and module-03 routes, enable header nav

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,17 +1,16 @@
 import { createRootRoute, createRoute, createRouter, redirect } from '@tanstack/react-router'
 import { RootLayout } from '@/app/layout/RootLayout'
 import { Module01Page } from '@/features/module-01/Module01Page'
+import { Module02Page } from '@/features/module-02/Module02Page'
+import { PokemonDetailPage } from '@/features/module-02/PokemonDetailPage'
+import { Module03Page } from '@/features/module-03/Module03Page'
 
-const rootRoute = createRootRoute({
-  component: RootLayout,
-})
+const rootRoute = createRootRoute({ component: RootLayout })
 
 const indexRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: '/',
-  beforeLoad: () => {
-    throw redirect({ to: '/module/01' })
-  },
+  beforeLoad: () => { throw redirect({ to: '/module/01' }) },
 })
 
 export const module01Route = createRoute({
@@ -20,12 +19,34 @@ export const module01Route = createRoute({
   component: Module01Page,
 })
 
-const routeTree = rootRoute.addChildren([indexRoute, module01Route])
+export const module02Route = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/module/02',
+  component: Module02Page,
+})
+
+export const module02DetailRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/module/02/pokemon/$name',
+  component: PokemonDetailPage,
+})
+
+export const module03Route = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/module/03',
+  component: Module03Page,
+})
+
+const routeTree = rootRoute.addChildren([
+  indexRoute,
+  module01Route,
+  module02Route,
+  module02DetailRoute,
+  module03Route,
+])
 
 export const router = createRouter({ routeTree })
 
 declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
+  interface Register { router: typeof router }
 }
